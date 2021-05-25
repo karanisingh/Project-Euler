48f66aebe5efabbb30d92919c16b18827211a569
Karan Singh <vbmsingh122@gmail.com>
2021-05-24 23:19:03 -0400

    Add project files.

---
+++ Solution.cpp
@@ -0,0 +1,16 @@
+// Solution.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <chrono>                  // Will use high_resolutio_timer to get duration of algorithm
+
+int main()
+{
+	auto start = std::chrono::high_resolution_clock::now();
+	//auto returnValue =           //this line is used to test specific solutions' functions
+	auto end = std::chrono::high_resolution_clock::now();
+	auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
+	std::cout << returnValue << "\n" << duration.count() << " milliseconds." << std::endl;
+
+	return 0;
+}
